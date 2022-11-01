970ffc341d9a204f6248777a09561b0835760561
dantehays <HAYSDC@ETSU.EDU>
2022-11-01 09:31:43 -0400

    Please

---
+++ Team 3 BucHunt/Team_3_BucHunt_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Team_3_BucHunt_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
